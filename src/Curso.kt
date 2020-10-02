cb414d824e24af557cbeb93488e42c727be8fe1e
Lais Dias <laisdias099@gmail.com>
2020-10-02 12:02:12 -0300

    removendo construtor secundário desnecessário e adicionando toString

--- src/Curso.kt
+++ src/Curso.kt
@@ -3,22 +3,9 @@ class Curso (
         val codigoCurso: Int,
         var quantidadeMaximaDeAlunos: Int
 ) {
-    var professorTitular: ProfessorTitular = ProfessorTitular("", "", 0, 0, "")
-    var professorAdjunto: ProfessorAdjunto = ProfessorAdjunto("", "", 0, 0, 0)
+    var professorTitular: ProfessorTitular? = null
+    var professorAdjunto: ProfessorAdjunto? = null
     var alunosMatriculados: MutableList<Aluno> = mutableListOf<Aluno>()
-
-    constructor(
-            nome: String,
-            codigoCurso: Int,
-            quantidadeMaximaDeAlunos: Int,
-            professorTitular: ProfessorTitular,
-            professorAdjunto: ProfessorAdjunto,
-            alunosMatriculados: MutableList<Aluno>
-    ): this(nome, codigoCurso, quantidadeMaximaDeAlunos){
-        this.professorTitular = professorTitular
-        this.professorAdjunto = professorAdjunto
-        this.alunosMatriculados = alunosMatriculados
-    }
 
     fun adicionarUmAluno(umAluno: Aluno): Boolean{
         if(this.alunosMatriculados.size < this.quantidadeMaximaDeAlunos){
@@ -30,5 +17,9 @@ class Curso (
 
     fun excluirAluno(umAluno: Aluno){
         this.alunosMatriculados.remove(umAluno)
+    }
+
+    override fun toString(): String {
+        return "Curso(nome='$nome', codigoCurso=$codigoCurso, quantidadeMaximaDeAlunos=$quantidadeMaximaDeAlunos, professorTitular=$professorTitular, professorAdjunto=$professorAdjunto, alunosMatriculados=$alunosMatriculados)"
     }
 }
