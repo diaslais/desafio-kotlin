8c0fe0ce1d7193d4097ff3fc7140c954c96e44ed
Lais Dias <laisdias099@gmail.com>
2020-10-02 12:01:15 -0300

    adicionando função toString

--- src/Aluno.kt
+++ src/Aluno.kt
@@ -2,4 +2,8 @@ class Aluno(
         val nome: String,
         val sobrenome: String,
         val codigoAluno: Int
-)
+){
+    override fun toString(): String {
+        return "Aluno(nome='$nome', sobrenome='$sobrenome', codigoAluno=$codigoAluno)"
+    }
+}
